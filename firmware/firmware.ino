67338fa327cc87f502a101efacdb61232e5f9ba3
Laszlo Domoszlai <l.domoszlai@science.ru.nl>
2017-07-15 13:42:48 +0200

    add prototype framework for firmware/client communication

---
+++ firmware/firmware.ino
@@ -0,0 +1,90 @@
+#include "PacketSerial.h"
+
+// The size of the queue in number of commands
+#define QUEUE_SIZE  1000
+
+struct cmd
+{
+  uint8_t left_motor;
+  uint8_t right_motor;
+  uint8_t pen_on;
+};
+
+// It is going to be a circular buffer
+struct cmd cmd_queue[QUEUE_SIZE];
+// Pointer to the next free slot
+int queue_ptr = 0;
+// The number of elements (steps) in the queue
+int nr_cmds = 0;
+
+// For communicating using COBS
+PacketSerial serial;
+
+uint8_t cmdsRequested;
+
+void requestCmds()
+{
+  uint8_t tmp[1]; 
+
+  if(QUEUE_SIZE - nr_cmds > 255)
+  {
+    tmp[0] = 255;
+  }
+  else
+  {
+    tmp[0] = QUEUE_SIZE - nr_cmds;
+  }
+
+  cmdsRequested = tmp[0];  
+  if(cmdsRequested>0) serial.send(tmp, 1);
+}
+
+void setup()
+{
+  // We must specify a packet handler method so that
+  serial.setPacketHandler(&onPacket);
+  serial.begin(115200);
+
+  // Ask for commands
+  requestCmds();
+}
+
+void loop()
+{
+  // The update() method should be called at the end of the loop().
+  serial.update(); 
+}
+
+// This is our packet callback.
+// The buffer is delivered already decoded.
+void onPacket(const uint8_t* buffer, size_t size)
+{
+  // Make a temporary buffer.
+  uint8_t tmp[size]; 
+  
+  // Copy the packet into our temporary buffer.
+  memcpy(tmp, buffer, size); 
+  
+  addCmd(tmp, size);
+}
+
+// Add the step to the queue and request more steps if this was the last expected one.
+void addCmd(uint8_t* buffer, size_t size)
+{
+  noInterrupts();
+
+  cmd_queue[queue_ptr].left_motor = buffer[0];
+  cmd_queue[queue_ptr].right_motor = buffer[1];
+  cmd_queue[queue_ptr].pen_on = buffer[2];
+
+  queue_ptr = queue_ptr++ % QUEUE_SIZE;
+  nr_cmds++;
+
+  if(--cmdsRequested == 0)
+  {
+    requestCmds();      
+  }
+
+  interrupts();
+}
+
---
+++ sendsteps.js
@@ -0,0 +1,118 @@
+var fs = require('fs');
+var LineReader = require('linereader');
+
+var SerialPort = require('serialport');
+var Delimiter = SerialPort.parsers.Delimiter;
+var cobs = require('cobs');
+
+var argv = require("argv");
+
+var args = argv.option([
+    {
+        name: 'scan',
+        short: 's',
+        type: 'boolean'
+    },
+    {
+        name: 'port',
+        short: 'p',
+        type: 'string'
+    },
+    {
+        name: 'baud',
+        short: 'b',
+        type: 'int'
+    },
+    {
+        name: 'help',
+        short: 'h',
+        type: 'boolean'
+    }        
+]).run();
+
+
+if(args.options.detect)
+{
+    detect();
+}
+else if(args.targets.length == 1 && args.options.port)
+{
+    var br = 115200;
+    if(args.options.baudrate) br = args.options.baudrate;
+    
+    main(args.targets[0], args.options.port, br);
+}
+else
+{    
+   console.info('Usage: sendsteps.js <stepfile> -p portname [options] \n\
+\n\
+        --help, -h \n\
+                Displays help information about this script \n\
+\n\
+        --port portname, -p portname\n\
+                Serial port to use \n\
+\n\
+        --baud baudrate, -b baudrate\n\
+                 Baudrate. Default 115200 \n\
+\n\
+        --scan, -s \n\
+                List available serial ports');
+}
+
+function detect()
+{
+    SerialPort.list(function (err, ports) {
+        ports.forEach(function(port) {
+            console.log(port.comName + " (" + port.manufacturer + ")");
+        });
+    });
+}
+
+var lastId = 0;
+
+function test(port)
+{
+    function sendSteps(nrSteps) {
+        for(var i=0; i<nrSteps; i++)
+        {
+            lastId++;
+            port.write(cobs.encode(new Buffer([lastId, lastId, lastId])));
+            port.write(new Buffer([0x00]));
+            
+            console.log('Send: ', new Buffer([lastId, lastId, lastId]));
+        }
+    }
+
+    var parser = port.pipe(new Delimiter({delimiter: new Buffer([0x00])}));
+    parser.on('data', function (data) {
+        var nrSteps = cobs.decode(data)[0];
+        console.log('Steps requested: ', cobs.decode(data));
+        sendSteps(nrSteps);
+    });
+}
+
+function main(stepfile, portname, baudrate)
+{    
+    fs.readFile(stepfile, 'utf8', function(err, data){    
+        if (err) 
+        {
+            return console.log('Error: ', err.message);
+        }
+        else
+        { 
+            var port = new SerialPort(portname, {
+                    baudRate: baudrate,
+                    autoOpen: false 
+            });
+                        
+            port.open(function (err) {
+                  if (err) {
+                    return console.log('Error: ', err.message);
+                  }
+    
+                  test(port);  
+                  //port.write(Cobs.encode(new Buffer([0x11,0x22,0x00,0x33])));                  
+            });            
+        }
+    });
+}
