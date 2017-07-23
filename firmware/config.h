03ba53a9cfa025eea94fae9c08e3da5b64a1a65a
Laszlo Domoszlai <l.domoszlai@science.ru.nl>
2017-07-23 22:10:09 +0200

    set actual pins

--- firmware/config.h
+++ firmware/config.h
@@ -1,15 +1,15 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define LEFT_STEP_PIN         54
-#define LEFT_DIR_PIN          55
-#define LEFT_ENABLE_PIN       38 
+#define LEFT_STEP_PIN         46  // Ramps Z
+#define LEFT_DIR_PIN          48
+#define LEFT_ENABLE_PIN       62 
 
-#define RIGHT_STEP_PIN        60
+#define RIGHT_STEP_PIN        60  // Ramps Y
 #define RIGHT_DIR_PIN         61
 #define RIGHT_ENABLE_PIN      56 
 
-#define PEN_PIN               4
+#define PEN_PIN               4     // 5, 6, 11
 #define PEN_CHANGE_TIME       20    // ms
 #define PEN_ON_ANGLE          0     
 #define PEN_OFF_ANGLE         20
